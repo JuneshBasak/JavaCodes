7246bd8956b5fea6cfeeb2a6670a66a99b263118
Junesh Basak <junesh.ms@gmail.com>
2020-07-30 15:32:09 +0530

    First edition

---
+++ HelloWorld.java
@@ -0,0 +1,27 @@
+public class Hello {
+    //Print Hello World & Hello Junesh!
+    public static void main (String[] arg){
+
+        System.out.println ("Hello World");
+
+    //Print Hello Junesh!
+
+        System.out.println("Hello Junesh!");
+
+        //Print an integer value
+
+        int MyIntegerValue = 5;
+        int MyIntegerValue2 = MyIntegerValue * 3;
+        int MyIntegerValue3 = MyIntegerValue2 *2 ;
+
+        int MyTotalValue = MyIntegerValue + MyIntegerValue2 + MyIntegerValue3;
+
+        System.out.println(MyTotalValue);
+
+        int MyLastOne = 1000 - MyTotalValue;
+        System.out.println(MyLastOne);
+
+
+    }
+
+}
